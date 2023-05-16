8492c844669307fdc390ade53ac0be5a205fa862
Ben Zeise <ben.zeise@gmail.com>
2023-05-16 17:59:52 -0500

    create book page

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -28,6 +28,11 @@ namespace mvc_project.Controllers
             return View(objBookList);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
